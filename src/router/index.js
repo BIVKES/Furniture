ad1dde91f4ba0fa0b40838704714bd5531fa4ed1
Артём <artem@MacBook-Pro-Artem.local>
2026-06-15 15:23:47 +0300

    Add GitHub Pages deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/pages/Home/Home.vue'
 
 const routes = [
@@ -45,7 +45,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('/Furniture/'),
   routes,
   scrollBehavior() {
     return { top: 0 }
